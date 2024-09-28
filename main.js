db157544fc883f4fa2be05cbd9d9c6474f854e0f
Edgar Hernandez <143291407+EdgarHernandez1234@users.noreply.github.com>
2024-09-28 11:01:07 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+//reactNative.launchAndroidSimulator;
+//reactNative.launchIOSSimulator;
+import React from "react";
+import { StyleSheet, Text, View} from "react-native"
+print("Hello! Welcome to our Gardening Community  Management App!");
