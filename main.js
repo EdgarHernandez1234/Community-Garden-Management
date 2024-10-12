2da1fffebc16251ad7bf03778b93711bf7c90aea
Edgar Hernandez <143291407+EdgarHernandez1234@users.noreply.github.com>
2024-10-12 11:24:38 -0700

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-//reactNative.launchAndroidSimulator;
-//reactNative.launchIOSSimulator;
-import React from "react";
-import { StyleSheet, Text, View} from "react-native"
-print("Hello! Welcome to our Gardening Community  Management App!");
