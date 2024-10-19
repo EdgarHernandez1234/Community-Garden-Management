dc0db3dce1e2cc473948343718e9e9722c9783f6
Edgar Hernandez <143291407+EdgarHernandez1234@users.noreply.github.com>
2024-10-19 10:32:37 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
   console.log("App executed");
@@ -8,6 +8,8 @@ export default function App() {
       <Text> Community Garden Management</Text>
       <StatusBar style="auto" />
       <Image source={require('./assets/Logo.png')}/>
+      <Button color="#08540a" title="Create an account?" onPress={() => console.log("Yes account")}/>
+      <Button color="#838a54" title="continue as guest" onPress={() => console.log("No account")}/>
     </SafeAreaView>
   );
 }
@@ -20,3 +22,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
