ae713da2e8f167c178acc24f47f5eb300e5c9136
Edgar Hernandez <143291407+EdgarHernandez1234@users.noreply.github.com>
2024-11-16 11:07:24 -0800

    Create colors.js

---
+++ colors.js
@@ -0,0 +1,10 @@
+ const colors ={
+primary: '#00ad00',
+secondary: '#838a54',
+white: '#ffffff',
+black: '#000000',
+fgreen: '#00ad00',
+dgreen: '#838a54',
+yellow: '#FFA500'
+}
+export default colors;
